5a44c6ca9d8729c9a6aa00bbce2f29d68c8faf56
Jhyun Y <140397466+yjhorion@users.noreply.github.com>
2023-10-12 15:19:09 +0900

    Update app.py
    app.py 일부 코드 수정 ( id값 생성 함수 수정 )

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, url_for, request, redirect
-import random
+from bson.objectid import ObjectId 
 from pymongo import MongoClient
 
 app=Flask(__name__)
 
-client = MongoClient('')  # '' 안에 개인 몽고디비 주소넣으십시여
+client = MongoClient('') # 몽고db userid/password
 db = client['todo_db']
 collection = db['todos']
+
 
 
 @app.route("/todolist", methods = ["GET", "POST"])
 def home():
     if (request.method == "POST"):
         todo_name = request.form["todo_name"]
-        cur_id = random.randint(1, 1000)
         todo = {
-            'id': cur_id,
+            'id': ObjectId(),
             'name': todo_name,
             'checked': False,
         }
@@ -24,17 +24,17 @@ def home():
     todos = list(collection.find({}))
     return render_template("index.html", items=todos)
 
-@app.route("/checked/<int:todo_id>", methods=["POST"])
+@app.route("/checked/<string:todo_id>", methods=["POST"])
 def checked_todo(todo_id):
-    todo = collection.find_one({'id': todo_id})
+    todo = collection.find_one({'id': ObjectId(todo_id)})
     if todo:
         todo['checked'] = not todo['checked']
-        collection.update_one({'id': todo_id}, {"$set": {'checked': todo['checked']}})
+        collection.update_one({'id': ObjectId(todo_id)}, {"$set": {'checked': todo['checked']}})
     return redirect(url_for('home'))
 
-@app.route("/delete/<int:todo_id>", methods=["POST"])
+@app.route("/delete/<string:todo_id>", methods=["POST"])
 def delete_todo(todo_id):
-    collection.delete_one({'id': todo_id})
+    collection.delete_one({'id': ObjectId(todo_id)})
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
